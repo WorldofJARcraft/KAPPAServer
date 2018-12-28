d9ef5d6c3074a79fd0dc745cac1e3d917bc430af
Eric Ackermann <eric.ackermann.99@googlemail.com>
2018-12-28 18:07:47 +0100

    Added Docker database configuration.

--- Docker/Kappa-Docker/build.sh
+++ Docker/Kappa-Docker/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 cd ../..
+cp ./src/main/resources/application.properties ./src/main/resources/application.properties.old
+cp ./src/main/resources/docker.properties ./src/main/resources/application.properties
 mvn clean package
+mv ./src/main/resources/application.properties.old ./src/main/resources/application.properties
 cp ./target/*.jar ./Docker/Kappa-Docker/kappa.jar
 docker build -t kappa ./kappa.Dockerfile
