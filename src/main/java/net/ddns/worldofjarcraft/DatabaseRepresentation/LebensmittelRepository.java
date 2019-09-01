e57f57a2325ffbcf8ddf973ab233e531cc8d53e8
Eric Ackermann <eric.ackermann.99@googlemail.com>
2019-09-01 19:11:39 +0200

    Re-implemented search.

--- src/main/java/net/ddns/worldofjarcraft/DatabaseRepresentation/LebensmittelRepository.java
+++ src/main/java/net/ddns/worldofjarcraft/DatabaseRepresentation/LebensmittelRepository.java
@@ -1,10 +1,14 @@
 package net.ddns.worldofjarcraft.DatabaseRepresentation;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface LebensmittelRepository extends CrudRepository<Lebensmittel, Integer> {
+    @Query("select e from Lebensmittel e where name like CONCAT('%',:name,'%') and Besitzer = :userID")
+    public Lebensmittel[] searchByName(@Param("name") String name, @Param("userID") Benutzer user);
 }
--- src/main/java/net/ddns/worldofjarcraft/Schraenke/LebensmittelController.java
+++ src/main/java/net/ddns/worldofjarcraft/Schraenke/LebensmittelController.java
@@ -7,8 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
 
 @RestController
 public class LebensmittelController {
@@ -24,89 +24,125 @@ public class LebensmittelController {
     @Autowired
     private LebensmittelRepository lebensmittelRepository;
 
-    @RequestMapping(value = "/schrank/{id}/{fachID}/newLM",method = RequestMethod.POST)
+    @RequestMapping(value = "/schrank/{id}/{fachID}/newLM", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity newLM(HttpServletRequest request, @PathVariable int id, @PathVariable int fachID, @RequestParam String name, @RequestParam String Anzahl, @RequestParam Long haltbarkeit, @RequestParam Long eingelagert){
+    public ResponseEntity newLM(HttpServletRequest request, @PathVariable int id, @PathVariable int fachID, @RequestParam String name, @RequestParam String Anzahl, @RequestParam Long haltbarkeit, @RequestParam Long eingelagert) {
         String user = request.getRemoteUser();
-        Benutzer benutzer = users.findById(user).isPresent()? users.findById(user).get() : null;
-        if(benutzer != null) {
-            Kuehlschrank schrank = kuehlschrankRepository.findById(id).isPresent()? kuehlschrankRepository.findById(id).get() : null;
-            if(schrank!=null && schrank.getBesitzer().equals(benutzer)){
+        Benutzer benutzer = users.findById(user).isPresent() ? users.findById(user).get() : null;
+        if (benutzer != null) {
+            Kuehlschrank schrank = kuehlschrankRepository.findById(id).isPresent() ? kuehlschrankRepository.findById(id).get() : null;
+            if (schrank != null && schrank.getBesitzer().equals(benutzer)) {
                 Fach fach = faecher.findById(fachID).orElse(null);
-                if(fach!=null && fach.getKuehlschrank().equals(schrank)) {
+                if (fach != null && fach.getKuehlschrank().equals(schrank)) {
                     Lebensmittel lebensmittel = new Lebensmittel(name, Anzahl, haltbarkeit, fach, benutzer, eingelagert);
                     lebensmittelRepository.save(lebensmittel);
-                    return new ResponseEntity<>(lebensmittel.getNummer(),HttpStatus.CREATED);
+                    return new ResponseEntity<>(lebensmittel.getNummer(), HttpStatus.CREATED);
                 }
             }
         }
         ErrorClass error = new ErrorClass("Unauthorized!");
-        return new ResponseEntity<ErrorClass>(error,HttpStatus.UNAUTHORIZED);
-    }
-    @RequestMapping(value = "/schrank/{id}/{fachID}/getAll",method = RequestMethod.GET)
-    @ResponseBody
-    public ResponseEntity getLM(HttpServletRequest request, @PathVariable int id, @PathVariable int fachID){
-        String user = request.getRemoteUser();
-        Benutzer benutzer = users.findById(user).isPresent()? users.findById(user).get() : null;
-        if(benutzer != null) {
-            Kuehlschrank schrank = kuehlschrankRepository.findById(id).isPresent()? kuehlschrankRepository.findById(id).get() : null;
-            if(schrank!=null && schrank.getBesitzer().equals(benutzer)){
-                Fach fach = faecher.findById(fachID).orElse(null);
-                if(fach!=null && fach.getKuehlschrank().equals(schrank)) {
-                    List<Lebensmittel> lm = Lebensmittel.getAll(lebensmittelRepository, fach);
-                    return new ResponseEntity<>(lm,HttpStatus.OK);
-                }
-            }
-        }
-        ErrorClass error = new ErrorClass("Unauthorized!");
-        return new ResponseEntity<ErrorClass>(error,HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<ErrorClass>(error, HttpStatus.UNAUTHORIZED);
     }
 
-    @RequestMapping(value = "/schrank/{id}/{fachID}/{lebensmittelID}/delete",method = RequestMethod.DELETE)
+    @RequestMapping(value = "/schrank/{id}/{fachID}/getAll", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity deleteLM(HttpServletRequest request, @PathVariable int id, @PathVariable int fachID, @PathVariable int lebensmittelID){
+    public ResponseEntity getLM(HttpServletRequest request, @PathVariable int id, @PathVariable int fachID) {
         String user = request.getRemoteUser();
-        Benutzer benutzer = users.findById(user).isPresent()? users.findById(user).get() : null;
-        if(benutzer != null) {
-            Kuehlschrank schrank = kuehlschrankRepository.findById(id).isPresent()? kuehlschrankRepository.findById(id).get() : null;
-            if(schrank!=null && schrank.getBesitzer().equals(benutzer)){
+        Benutzer benutzer = users.findById(user).isPresent() ? users.findById(user).get() : null;
+        if (benutzer != null) {
+            Kuehlschrank schrank = kuehlschrankRepository.findById(id).isPresent() ? kuehlschrankRepository.findById(id).get() : null;
+            if (schrank != null && schrank.getBesitzer().equals(benutzer)) {
                 Fach fach = faecher.findById(fachID).orElse(null);
-                if(fach!=null && fach.getKuehlschrank().equals(schrank)) {
+                if (fach != null && fach.getKuehlschrank().equals(schrank)) {
+                    List<Lebensmittel> lm = Lebensmittel.getAll(lebensmittelRepository, fach);
+                    return new ResponseEntity<>(lm, HttpStatus.OK);
+                }
+            }
+        }
+        ErrorClass error = new ErrorClass("Unauthorized!");
+        return new ResponseEntity<ErrorClass>(error, HttpStatus.UNAUTHORIZED);
+    }
+
+    @RequestMapping(value = "/schrank/{id}/{fachID}/{lebensmittelID}/delete", method = RequestMethod.DELETE)
+    @ResponseBody
+    public ResponseEntity deleteLM(HttpServletRequest request, @PathVariable int id, @PathVariable int fachID, @PathVariable int lebensmittelID) {
+        String user = request.getRemoteUser();
+        Benutzer benutzer = users.findById(user).isPresent() ? users.findById(user).get() : null;
+        if (benutzer != null) {
+            Kuehlschrank schrank = kuehlschrankRepository.findById(id).isPresent() ? kuehlschrankRepository.findById(id).get() : null;
+            if (schrank != null && schrank.getBesitzer().equals(benutzer)) {
+                Fach fach = faecher.findById(fachID).orElse(null);
+                if (fach != null && fach.getKuehlschrank().equals(schrank)) {
                     Lebensmittel lm = lebensmittelRepository.findById(lebensmittelID).isPresent() ? lebensmittelRepository.findById(lebensmittelID).get() : null;
-                    if(lm != null){
+                    if (lm != null) {
                         lebensmittelRepository.delete(lm);
                     }
-                    return new ResponseEntity<>("",HttpStatus.ACCEPTED);
+                    return new ResponseEntity<>("", HttpStatus.ACCEPTED);
                 }
             }
         }
         ErrorClass error = new ErrorClass("Unauthorized!");
-        return new ResponseEntity<ErrorClass>(error,HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<ErrorClass>(error, HttpStatus.UNAUTHORIZED);
     }
 
-    @RequestMapping(value = "/schrank/{id}/{fachID}/{lebensmittelID}/update",method = RequestMethod.POST)
+    @RequestMapping(value = "/schrank/{id}/{fachID}/{lebensmittelID}/update", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity updateLM(HttpServletRequest request, @PathVariable int id, @PathVariable int fachID, @PathVariable int lebensmittelID, @RequestParam String name, @RequestParam String Anzahl, @RequestParam Long haltbarkeit, @RequestParam Long eingelagert){
+    public ResponseEntity updateLM(HttpServletRequest request, @PathVariable int id, @PathVariable int fachID, @PathVariable int lebensmittelID, @RequestParam String name, @RequestParam String Anzahl, @RequestParam Long haltbarkeit, @RequestParam Long eingelagert) {
         String user = request.getRemoteUser();
-        Benutzer benutzer = users.findById(user).isPresent()? users.findById(user).get() : null;
-        if(benutzer != null) {
-            Kuehlschrank schrank = kuehlschrankRepository.findById(id).isPresent()? kuehlschrankRepository.findById(id).get() : null;
-            if(schrank!=null && schrank.getBesitzer().equals(benutzer)){
+        Benutzer benutzer = users.findById(user).isPresent() ? users.findById(user).get() : null;
+        if (benutzer != null) {
+            Kuehlschrank schrank = kuehlschrankRepository.findById(id).isPresent() ? kuehlschrankRepository.findById(id).get() : null;
+            if (schrank != null && schrank.getBesitzer().equals(benutzer)) {
                 Fach fach = faecher.findById(fachID).orElse(null);
-                if(fach!=null && fach.getKuehlschrank().equals(schrank)) {
+                if (fach != null && fach.getKuehlschrank().equals(schrank)) {
                     Lebensmittel lm = lebensmittelRepository.findById(lebensmittelID).isPresent() ? lebensmittelRepository.findById(lebensmittelID).get() : null;
-                    if(lm != null){
+                    if (lm != null) {
                         lm.setName(name);
                         lm.setAnzahl(Anzahl);
                         lm.setHaltbarkeitsdatum(haltbarkeit);
                         lm.setEingelagert(eingelagert);
                         lebensmittelRepository.save(lm);
                     }
-                    return new ResponseEntity<>("",HttpStatus.ACCEPTED);
+                    return new ResponseEntity<>("", HttpStatus.ACCEPTED);
                 }
             }
         }
         ErrorClass error = new ErrorClass("Unauthorized!");
-        return new ResponseEntity<ErrorClass>(error,HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<ErrorClass>(error, HttpStatus.UNAUTHORIZED);
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity searchLM(HttpServletRequest request, @RequestParam String query) {
+        String user = request.getRemoteUser();
+        Benutzer benutzer = users.findById(user).isPresent() ? users.findById(user).get() : null;
+        if (benutzer != null) {
+            Set<Lebensmittel> results = new HashSet<>();
+
+            for (Lebensmittel lm : lebensmittelRepository.findAll()) {
+                try {
+                    String queryDecoded = query;
+                    if(query.startsWith("Base64:")){
+                     queryDecoded = new String(Base64.getMimeDecoder().decode(query.split("Base64:")[1]), "utf-8");
+                    }
+                    if (lm.getName().startsWith("Base64:")) {
+                        String lm_decoded = lm.getName().split("Base64:")[1];
+                        lm_decoded = new String(Base64.getMimeDecoder().decode(lm_decoded), "utf-8");
+                        if (lm_decoded.contains(queryDecoded) && lm.getBesitzer().equals(benutzer)) {
+                            results.add(lm);
+                        }
+                    } else if(lm.getName().contains(query) && lm.getBesitzer().equals(benutzer)){
+                        results.add(lm);
+                    }
+
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            return new ResponseEntity<>(results, HttpStatus.OK);
+        }
+        ErrorClass error = new ErrorClass("Unauthorized!");
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
 }
